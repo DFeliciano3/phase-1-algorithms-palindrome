dadd958e84c8bb08f4a71935d7cd23189c1ee844
DFeliciano3 <91075867+DFeliciano3@users.noreply.github.com>
2022-06-22 12:44:38 -0400

    commit tree 1aa55421edd3bf32964a9c497deae1bd72294101

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-function isPalindrome(word) {
+function isPalindrome(str) {
+  return str.split('').every((CharacterData, i) => {
+    return CharacterData === str[str.length - i - 1]; 
+  });
   // Write your algorithm here
 }
 
 /* 
   Add your pseudocode here
+  
 */
 
 /*
@@ -14,12 +18,19 @@ function isPalindrome(word) {
 if (require.main === module) {
   // add your own custom tests in here
   console.log("Expecting: true");
+
+  console.log("=>", isPalindrome("mom")); 
+  console.log("=>", isPalindrome("noon"));
+  console.log("=>", isPalindrome("rotator"));
   console.log("=>", isPalindrome("racecar"));
 
   console.log("");
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+  console.log("=>", isPalindrome("today"));
+  console.log("=>", isPalindrome("now"));
+  console.log("=>", isPalindrome("sam"));
 }
 
 module.exports = isPalindrome;
